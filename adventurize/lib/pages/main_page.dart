17a977f9c08198307bd086adaa524d78a04e9d43
eleninspl <eleni.nspl@gmail.com>
2024-12-28 12:17:08 +0200

    add bottom buttons

--- adventurize/lib/pages/main_page.dart
+++ adventurize/lib/pages/main_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:adventurize/components/bottom_nav_component.dart';
+import 'package:adventurize/components/bottom_buttons.dart';
 import 'package:adventurize/pages/challenges_page.dart';
 import 'package:adventurize/pages/leaderboard_page.dart';
 import 'package:adventurize/pages/memory_history_page.dart';
@@ -10,26 +10,13 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _pages = [
-    ChallengesPage(),
-    LeaderboardPage(),
-    MemoryHistoryPage(),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavComponent(
-        currentIndex: _currentIndex, // Pass current index
-        onItemSelected: (index) {
-          setState(() {
-            _currentIndex = index; // Update index
-          });
-        },
+      appBar: AppBar(
+        title: const Text('Main Page'),
       ),
+      bottomNavigationBar: BottomButtons(),
     );
   }
 }
